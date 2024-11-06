081d7bc4faeba388281fa36ea26b860c4fdf2a64
mariajosecq <cote21cq@gmail.com>
2024-11-06 13:37:20 -0300

    Se corrige bug en fecha y hora de editar campaña de la web

--- web-mensajeria-masiva/dist/proyecto_arq/server/main.server.mjs
+++ web-mensajeria-masiva/dist/proyecto_arq/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KVETIMO6.mjs";import"./chunk-F477LJB2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-MOJWPLU6.mjs";import"./chunk-F477LJB2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- web-mensajeria-masiva/src/app/modal/edit-campana/edit-campana.component.ts
+++ web-mensajeria-masiva/src/app/modal/edit-campana/edit-campana.component.ts
@@ -16,7 +16,20 @@ export class EditCampanaComponent {
   @Output() onSave = new EventEmitter<campana>();
   @Output() onClose = new EventEmitter<void>();
 
+  
+  currentDate: string;
+
   tempNombre: string = '';
+
+  availableHours: string[] = [];
+
+  minDate: string = '';
+
+  constructor() {
+    const today = new Date();
+    this.currentDate = today.toISOString().split('T')[0]; // Obtener la fecha en formato YYYY-MM-DD
+    this.generateAvailableHours();//Obtener horas en el rango disponible
+  }  
 
   ngOnInit() {
     // Al inicializar, convierte el nombre a capitalizado
@@ -25,11 +38,27 @@ export class EditCampanaComponent {
       .split(' ')
       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ');
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+    this.minDate = tomorrow.toISOString().split('T')[0];
   }
 
   save() {
     this.campana.nombre = this.tempNombre;
     this.onSave.emit(this.campana); // Emite los nuevos datos
+  }
+
+  generateAvailableHours() {
+    const startHour = 8; // 08:00
+    const endHour = 22; // 22:00
+
+    for (let hour = startHour; hour <= endHour; hour++) {
+      for (let minutes of [0, 15, 30, 45]) {
+        if (hour === endHour && minutes > 0) break; // Evitar horas después de 22:00
+        this.availableHours.push(`${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`);
+      }
+    }
   }
   
 }
